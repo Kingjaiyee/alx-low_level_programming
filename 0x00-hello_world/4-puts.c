78cf5b89bba3badead3cc40096348957f14a6a28
root <victordurojaiye2000@gmail.com>
2022-03-10 13:06:01 -0800

    This is my c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - entry point for our program
+ *Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming a mutilingual puzzle");
+	return (0);
+}
