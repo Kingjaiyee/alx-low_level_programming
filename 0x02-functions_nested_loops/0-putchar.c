95f688f71719d33b75e073e8afab2c5cdef2233f
root <victordurojaiye2000@gmail.com>
2022-03-15 08:16:44 -0700

    the best nested task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,22 @@
 #include "main.h"
 
 /**
- * main - function to print
- *
- * Return: 0
- */
+* main - Lanzador
+*
+* Return: Always 0
+*
+*/
+
 int main(void)
 {
-	char holberton[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
-	unsigned int c;
-
-	for (c = 0; c < sizeof(holberton); c++)
-	{
-		_putchar(holberton[c]);
-	}
-_putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
