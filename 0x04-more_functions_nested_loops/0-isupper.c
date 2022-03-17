8382a93e475a447f8ef240eac507aa001ff643fa
root <victordurojaiye2000@gmail.com>
2022-03-17 16:49:09 -0700

    I am tired

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,22 +1,16 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _isupper - checks for lowercase character.
- * @c: caracter recibo por main.
- * Return: 1 if c is uppercase
+ * _isupper - Check if paramter c represents an uppercase letter.
+ * @c: Int representing a character
+ * Return: Always 0.
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
-	else if (c <= 'z')
-	{
-		return (0);
-	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
